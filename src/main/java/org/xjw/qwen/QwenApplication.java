af1404ff064c31ba149136ecd140b445bb6e8c66
oishfjasdn <2297886372@qq.com>
2025-08-17 15:37:10 +0800

    test bcken

---
+++ src/main/java/org/xjw/qwen/QwenApplication.java
@@ -0,0 +1,13 @@
+package org.xjw.qwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QwenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xjw/qwen/QwenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xjw.qwen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
